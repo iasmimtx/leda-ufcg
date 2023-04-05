a22acc634d6cf2fcb1ddace5d31f57e9f69bef39
iasmimtx <iasmimt31@gmail.com>
2023-04-05 09:52:40 -0300

    insertionSort

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -1,6 +1,7 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * As the insertion sort algorithm iterates over the array, it makes the
@@ -12,6 +13,12 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		
+		for(int i = 1; i <= rightIndex; i++){
+			int j = i;
+			while(j > 0 && array[j].compareTo(array[j-1]) < 0) {
+				Util.swap(array, j, j-1);
+				j--;
+			}
+		}
 	}
 }
