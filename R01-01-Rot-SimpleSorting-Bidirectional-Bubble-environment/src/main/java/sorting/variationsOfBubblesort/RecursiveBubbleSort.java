27ddc34c6de815ff76c61923e501dec0478f99df
Iasmim Torres <iasmimt31@gmail.com>
2023-04-10 21:30:29 -0300

    finalizando roteiro sem a criação de testes :)

--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/RecursiveBubbleSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfBubblesort/RecursiveBubbleSort.java
@@ -1,22 +1,40 @@
 package sorting.variationsOfBubblesort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 public class RecursiveBubbleSort<T extends Comparable<T>> extends
-		AbstractSorting<T> {
+        AbstractSorting<T> {
 
-	/**
-	 * Implementação recursiva do bubble sort. Você deve implementar apenas esse
-	 * método sem usar nenhum outro método auxiliar (exceto
-	 * Util.swap(array,int,int)). Para isso, tente definir o caso base do
-	 * algoritmo e depois o caso indutivo, que reduz o problema para uma entrada
-	 * menor em uma chamada recursiva. Seu algoritmo deve ter complexidade
-	 * quadrática O(n^2).
-	 */
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
-	}
+    /**
+     * Implementação recursiva do bubble sort. Você deve implementar apenas esse
+     * método sem usar nenhum outro método auxiliar (exceto
+     * Util.swap(array,int,int)). Para isso, tente definir o caso base do
+     * algoritmo e depois o caso indutivo, que reduz o problema para uma entrada
+     * menor em uma chamada recursiva. Seu algoritmo deve ter complexidade
+     * quadrática O(n^2).
+     */
+    @Override
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+
+        if (isValidation(array, leftIndex, rightIndex)) {
+            if (leftIndex < rightIndex) {
+                //percorre o array e troca os valores se nao tiverem na devida ordem.
+                for (int i = leftIndex; i < rightIndex; i++) {
+                    if (array[i].compareTo(array[i + 1]) > 0) Util.swap(array, i, i + 1);
+                }
+
+                //caso recursivo
+                sort(array, leftIndex, rightIndex - 1);
+            }
+        }
+    }
+
+    private boolean isValidation(T[] array, int leftIndex, int rightIndex) {
+        boolean result = true;
+        if ((leftIndex > rightIndex || leftIndex < 0 || rightIndex < 0 || rightIndex > array.length || array.equals(null)))
+            result = false;
+        return result;
+    }
 
 }
--- R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfSelectionsort/RecursiveSelectionSort.java
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/variationsOfSelectionsort/RecursiveSelectionSort.java
@@ -1,22 +1,37 @@
 package sorting.variationsOfSelectionsort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 public class RecursiveSelectionSort<T extends Comparable<T>> extends
-		AbstractSorting<T> {
+        AbstractSorting<T> {
 
-	/**
-	 * Implementação recursiva do selection sort. Você deve implementar apenas
-	 * esse método sem usar nenhum outro método auxiliar (exceto
-	 * Util.swap(array,int,int)). Para isso, tente definir o caso base do
-	 * algoritmo e depois o caso indutivo, que reduz o problema para uma entrada
-	 * menor em uma chamada recursiva. Seu algoritmo deve ter complexidade
-	 * quadrática O(n^2).
-	 */
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
-	}
+    /**
+     * Implementação recursiva do selection sort. Você deve implementar apenas
+     * esse método sem usar nenhum outro método auxiliar (exceto
+     * Util.swap(array,int,int)). Para isso, tente definir o caso base do
+     * algoritmo e depois o caso indutivo, que reduz o problema para uma entrada
+     * menor em uma chamada recursiva. Seu algoritmo deve ter complexidade
+     * quadrática O(n^2).
+     */
+    @Override
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+        if (isValidation(array, leftIndex, rightIndex)) {
+            if (leftIndex < rightIndex) {
+                int menor = leftIndex;
+                for (int i = leftIndex + 1; i <= rightIndex; i++) {
+                    if (array[i].compareTo(array[menor]) < 0) menor = i;
+                }
+                Util.swap(array, leftIndex, menor);
+                sort(array, leftIndex + 1, rightIndex);
+            }
+        }
+    }
 
+    private boolean isValidation(T[] array, int leftIndex, int rightIndex) {
+        boolean result = true;
+        if ((leftIndex > rightIndex || leftIndex < 0 || rightIndex < 0 || rightIndex > array.length || array.equals(null)))
+            result = false;
+        return result;
+    }
 }
