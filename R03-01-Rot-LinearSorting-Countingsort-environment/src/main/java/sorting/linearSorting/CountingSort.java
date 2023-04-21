1374c585abfe25df9fd7e99a4e07ccef50d633e4
Iasmim Torres <iasmimt31@gmail.com>
2023-04-21 20:55:50 -0300

    r03

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -4,21 +4,51 @@ import sorting.AbstractSorting;
 
 /**
  * Classe que implementa a estratégia de Counting Sort vista em sala.
- *
+ * <p>
  * Procure evitar desperdício de memória: AO INVÉS de alocar o array de contadores
  * com um tamanho arbitrariamente grande (por exemplo, com o maior valor de entrada possível),
  * aloque este array com o tamanho sendo o máximo inteiro presente no array a ser ordenado.
- *
+ * <p>
  * Seu algoritmo deve assumir que o array de entrada nao possui numeros negativos,
  * ou seja, possui apenas numeros inteiros positivos e o zero.
- *
  */
 public class CountingSort extends AbstractSorting<Integer> {
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void sort(Integer[] array, int leftIndex, int rightIndex) {
+        if (isValidation(array, leftIndex, rightIndex)) {
+            int max = leftIndex;
+            //verificando qual é o número maior do array
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                if (array[i] > max) max = array[i];
+            }
 
+            int[] C = new int[max + 1];
+            //frequencia
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                C[array[i]] += 1;
+            }
+
+            //cumulativa
+            for (int i = 1; i < C.length; i++) {
+                C[i] += C[i - 1];
+            }
+
+            int[] B = new int[rightIndex - leftIndex + 1];
+
+            for (int i = rightIndex; i >= leftIndex; i--) {
+                B[C[array[i]] - 1] = array[i];
+                C[array[i]] -= 1;
+            }
+            int index = 0;
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                array[i] = B[index];
+                index++;
+            }
+        }
+    }
+
+	private boolean isValidation(Integer[] array, int leftIndex, int rightIndex) {
+		return array != null && leftIndex >= 0 && leftIndex < rightIndex && rightIndex < array.length;
+	}
 }
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -10,10 +10,61 @@ import sorting.AbstractSorting;
  */
 public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
-	@Override
-	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    @Override
+    public void sort(Integer[] array, int leftIndex, int rightIndex) {
+        if (isValidation(array, leftIndex, rightIndex)) {
+            int max = max(array, leftIndex, rightIndex);
+            int min = min(array, leftIndex, rightIndex);
+
+
+            int[] C = new int[max - min + 1];
+            //frequencia
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                C[array[i] - min] += 1;
+            }
+
+            //cumulativa
+            for (int i = 1; i < C.length; i++) {
+                C[i] += C[i - 1];
+            }
+
+            int[] B = new int[rightIndex - leftIndex + 1];
+
+            for (int i = rightIndex; i >= leftIndex; i--) {
+                B[C[array[i] - min] - 1] = array[i];
+                C[array[i] - min] -= 1;
+            }
+
+            int indice = 0;
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                array[i] = B[indice];
+                indice++;
+
+            }
+        }
+    }
+
+    private boolean isValidation(Integer[] array, int leftIndex, int rightIndex) {
+        return array != null && leftIndex >= 0 && leftIndex < rightIndex && rightIndex < array.length;
+    }
+
+    private int min(Integer[] array, int leftIndex, int rightIndex) {
+        int min = leftIndex;
+        //verificando qual é o número menor do array
+        for (int i = leftIndex; i <= rightIndex; i++) {
+            if (array[i] < min) min = array[i];
+        }
+        return min;
+    }
+
+    private int max(Integer[] array, int leftIndex, int rightIndex) {
+        int max = leftIndex;
+        //verificando qual é o número maior do array
+        for (int i = leftIndex; i <= rightIndex; i++) {
+            if (array[i] > max) max = array[i];
+        }
+        return max;
+    }
+
 
 }
